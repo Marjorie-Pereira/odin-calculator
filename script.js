f1c3d095db0c02cb683cc3bdc56c6b7c46494c71
Marjorie-Pereira <marjorie300904@gmail.com>
2026-06-25 21:18:32 -0300

    Create operate function to calculate

--- script.js
+++ script.js
@@ -14,3 +14,25 @@ function divide(operand1, operand2) {
   return operand1 / operand2;
 }
 
+const num1;
+const num2;
+const operator;
+
+function operate(operator, operand1, operand2) {
+  switch(operator) {
+    case '+':
+      return add(operand1, operand2);
+      break;
+    case '-':
+      return subtract(operand1, operand2);
+      break;
+    case '*':
+      return multiply(operand1, operand2);
+      break;
+    case '/':
+      return divide(operand1, operand2);
+      break;
+    default:
+      return 'Invalid operator';
+  }
+}
