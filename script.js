8d311cab2282efd2f818b31fe05854f52bb40249
Marjorie-Pereira <marjorie300904@gmail.com>
2026-06-26 18:13:25 -0300

    Remove dead code

--- script.js
+++ script.js
@@ -23,16 +23,12 @@ function operate(operator, operand1, operand2) {
   switch (operator) {
     case "+":
       return add(operand1, operand2);
-      break;
     case "-":
       return subtract(operand1, operand2);
-      break;
     case "*":
       return multiply(operand1, operand2);
-      break;
     case "/":
       return divide(operand1, operand2);
-      break;
     default:
       return "Invalid operator";
   }
